7ff2f96d6d33da2ef38b0c8a1e67bd46567d8cca
shreyasMunge7 <shreyasmunge@gmail.com>
2024-07-24 21:21:36 +0530

    add-backend

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    available = models.BooleanField(default=True)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    ordered_at = models.DateTimeField(auto_now_add=True)
